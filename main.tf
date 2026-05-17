e775579468231a7713ec167c906e445cb98da9ce
Milton Munoz <livemilton@gmail.com>
2026-05-17 16:36:07 -0500

    Fullstack app digital twin: :sparkles: infraestructure - Automating AI deployments with Terraform and Shell scripts

--- main.tf
+++ main.tf
@@ -172,9 +172,10 @@ resource "aws_apigatewayv2_stage" "default" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
-  api_id           = aws_apigatewayv2_api.main.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.api.invoke_arn
+  api_id                 = aws_apigatewayv2_api.main.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.api.invoke_arn
+  payload_format_version = "2.0"
 }
 
 # API Gateway Routes
